b6f00ad71c2d7f1827185b8b0c67f4162f8be7c2
Pankaj Panday <ppanday064@gmail.com>
2024-07-03 17:52:57 +0530

    final commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
+import { ScrollRestoration } from "react-router-dom";
 
 const MainLayout = () => {
 	return (
 		<>
+			<ScrollRestoration />
 			<Header />
 			<Outlet />
 			<Footer />
